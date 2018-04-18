d9f4a45a30320a5e06248eb907c98d505379a5dc
wk-j <somnuk.wk@gmail.com>
2018-04-18 15:55:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Clipboard
+
+Copy console output to Clipboard without new line
+
+- [x] mac OS
+- [ ] Linux
+- [ ] Windows
+
+## Installation
+
+```
+dotnet install tool -g wk.Clipboard
+```
+
+## Usage
+
+```
+echo "Hello, world!" | wk-cb
+```
---
+++ src/Clipboard/Program.fs
@@ -0,0 +1,36 @@
+﻿module Clipboard
+
+open System
+open System.Diagnostics
+
+let run fileName arguments =
+    let  info  = ProcessStartInfo()
+    info.FileName <- fileName
+    info.Arguments <- arguments
+    info.RedirectStandardOutput <- true
+    info.UseShellExecute <- false
+    info.CreateNoWindow <- true
+
+    use ps = new Process()
+    ps.StartInfo <- info
+    ps.Start() |> ignore
+    let result = ps.StandardOutput.ReadToEnd()
+    ps.WaitForExit()
+    result
+
+let bash (cmd: string) =
+    let escapedArgs = cmd.Replace("\"", "\\\"")
+    run "/bin/bash"  (sprintf "-c \"%s\"" escapedArgs)
+
+let copy (value: string) =
+    (sprintf "echo \"%s\" | tr -d '\n' | pbcopy" value)
+    |> bash
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length <> 0 then
+        copy argv.[0]  |> ignore
+    else
+        let data = Console.ReadLine()
+        copy data |> ignore
+    0
